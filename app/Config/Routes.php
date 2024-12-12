c746cbf7418a236c84c34f41a8f241c3b8853660
Eduardo <eduardotrevezani@gmail.com>
2024-12-11 23:52:47 -0300

    fix: ajuste

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,6 +7,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->get('/', [AuthController::class, 'login']);
 $routes->get('/login', [AuthController::class, 'login']);
 $routes->get('/register', [AuthController::class, 'register']);
 $routes->post('/create', [AuthController::class, 'create']);
